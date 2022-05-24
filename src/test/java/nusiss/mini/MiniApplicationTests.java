267aafe9d716650ce7baea5c918b4b7c25c53617
tcFatt <chiunfatt@gmail.com>
2022-05-25 00:36:05 +0800

    third commit

--- src/test/java/nusiss/mini/MiniApplicationTests.java
+++ src/test/java/nusiss/mini/MiniApplicationTests.java
@@ -1,7 +1,18 @@
 package nusiss.mini;
 
+import javax.servlet.http.HttpSession;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
+
+import nusiss.mini.filter.AuthenticationFilter;
 
 
 @SpringBootTest
@@ -11,5 +22,40 @@ class TestApplicationTests {
 	void contextLoads() {
 	}
 
+	private static final AuthenticationFilter filter = new AuthenticationFilter();
+
+	@Test
+	public void testDoFilterShouldNotSendRedirect() throws Exception {
+		MockHttpSession session = new MockHttpSession();
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+        MockFilterChain chain = new MockFilterChain();
+		session.setAttribute("username", "sun");
+    	request.setSession(session);
+		request.setRequestURI("/secure/home");
+		HttpSession sess = request.getSession();
+		String username = (String)sess.getAttribute("username");
+		filter.doFilter(request, response, chain);
+		assertEquals("sun", username);
+		assertEquals("/secure/home", request.getRequestURI().toString());
+		assertNotEquals("/", response.getRedirectedUrl());
+	}
+
+	@Test
+	public void testDoFilterShouldSendRedirect() throws Exception {
+		MockHttpSession session = new MockHttpSession();
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        MockFilterChain chain = new MockFilterChain();
+		session.setAttribute("username", null); // username is null
+		request.setSession(session);
+		filter.doFilter(request, response, chain);
+		assertEquals("/", response.getRedirectedUrl());
+	}
+
+	@Test
+	public void main() {
+	   MiniApplication.main(new String[] {});
+	}
 
 }
